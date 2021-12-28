8b40ddf483764230c1ee1e1da5a925a1c8501e54
Almat Akhat <fabiannsailor@gmail.com>
2021-12-28 22:59:59 +0600

    added logic, routing and memory

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/Fe4p3b/go-backend-coursework/internal/app/shortener"
+	"github.com/Fe4p3b/go-backend-coursework/internal/handlers"
+	"github.com/Fe4p3b/go-backend-coursework/internal/server"
+	"github.com/Fe4p3b/go-backend-coursework/internal/storage/memory"
+)
+
+func main() {
+	m := memory.New(map[string]string{})
+	s := shortener.New(m)
+	_ = handlers.NewHttpHandler(s)
+	server := server.New(":8080", nil)
+
+	log.Fatal(server.Start())
+}
---
+++ internal/app/shortener/shortener.go
@@ -0,0 +1,34 @@
+package shortener
+
+import (
+	"github.com/Fe4p3b/go-backend-coursework/internal/repositories"
+	"github.com/lithammer/shortuuid"
+)
+
+type ShortenerService interface {
+	Find(string) (string, error)
+	Store(string) (string, error)
+}
+
+type shortener struct {
+	r repositories.ShortenerRepository
+}
+
+func New(r repositories.ShortenerRepository) *shortener {
+	return &shortener{
+		r: r,
+	}
+}
+
+func (s *shortener) Find(url string) (string, error) {
+	return s.r.Find(url)
+}
+
+func (s *shortener) Store(url string) (string, error) {
+	uuid := shortuuid.New()
+	err := s.r.Save(uuid, url)
+	if err != nil {
+		return "", err
+	}
+	return uuid, nil
+}
